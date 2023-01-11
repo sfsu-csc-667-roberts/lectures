4c18d79e1cef9495f3493e976b6451a7f1ab6669
John Roberts <roberts.john@gmail.com>
2023-01-10 17:45:27 -0800

    wip

--- lecture-one/SimpleServer.java
+++ lecture-one/SimpleServer.java
@@ -4,50 +4,70 @@ import java.io.*;
 public class SimpleServer {
   public static final int DEFAULT_PORT = 3100;
 
-  public static void main( String[] args ) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-    ServerSocket socket = new ServerSocket( DEFAULT_PORT );
-    Socket client = null;
+    ServerSocket socket = new ServerSocket(DEFAULT_PORT);
 
-    while( true ) {
-      client = socket.accept();
-      outputRequest( client );
-      sendResponse( client );
-      client.close();
+    while (true) {
+      new Thread(
+        new Worker(socket.accept())
+      ).start();
     }
   }
 
-  protected static void outputRequest( Socket client ) throws IOException {
+}
+
+class Worker implements Runnable {
+  private Socket socket;
+
+  public Worker(Socket socket) {
+    this.socket = socket;
+  }
+
+  @Override
+  public void run() {
+    try {
+      outputRequest(socket);
+      Thread.sleep((int)(Math.random()* 10000));
+      sendResponse(socket);
+      socket.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  protected void outputRequest(Socket client) throws IOException {
     String line;
 
-    BufferedReader reader = new BufferedReader(
-      new InputStreamReader( client.getInputStream() )
-    );
+    BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-    while( true ) {
+    while (true) {
       line = reader.readLine();
-      System.out.println( "> " + line );
+      System.out.println("> " + line);
 
       // Why do we need to do this?
-      if( line.contains( "END" ) ) {
+      if (line.contains("END")) {
         break;
       }
     }
     outputLineBreak();
   }
 
-  protected static void outputLineBreak() {
-    System.out.println( "-------------------------" );
+  protected void outputLineBreak() {
+    System.out.println("-------------------------");
   }
 
-  protected static void sendResponse( Socket client ) throws IOException {
-    PrintWriter out = new PrintWriter( client.getOutputStream(), true );
-    int gift = (int) Math.ceil( Math.random() * 100 );
+  protected void sendResponse(Socket client) throws IOException {
+    PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+    int gift = (int) Math.ceil(Math.random() * 100);
     String response = "Gee, thanks, this is for you: " + gift;
 
-    out.println( response );
+    out.println(response);
 
     outputLineBreak();
-    System.out.println( "I sent: " + response );
+    System.out.println("I sent: " + response);
   }
 }
